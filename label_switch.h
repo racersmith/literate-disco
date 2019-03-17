917790fa684dab834266277e2c466c15ce62b327
Josh Smith <racersmith@gmail.com>
2019-03-17 08:16:12 -0700

    adding method to check for labal change

--- label_switch.h
+++ label_switch.h
@@ -2,9 +2,11 @@
 
 class LabelSwitch{
   private:
-  int source_pin = 0;  // this pin will be used as a ground.  because, lazy.
+  int source_pin = 10;  // this pin will be used as a ground.  because, lazy.
   int n = 4;
-  int bit_pins[4] = {4, 3, 2, 1}; // in bit order
+  int bit_pins[4] = {6, 7, 8, 9}; // in bit order
+  
+  bool last_label = -1;
     
   public:
   LabelSwitch(){
@@ -16,12 +18,23 @@ class LabelSwitch{
     }
   }
 
-  int get_state(){
+  int get_label(){
     int label = 0;
     for (int i=0; i<n; i++){
       label |= !digitalRead(bit_pins[i]) << n-i-1;
     }
     return label;
+  }
+
+  bool is_new(){
+    int label = get_label();
+    if (last_label != label){
+      last_label = label;
+      return true;
+    }
+    else {
+      return false;
+    }
   }
   
 };
